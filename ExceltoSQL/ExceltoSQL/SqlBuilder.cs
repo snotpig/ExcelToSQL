835d26a3d0ee29d3125d90f3b3215d5e0d1b2089
snotpig <snotpig@users.noreply.github.com>
2019-02-27 22:37:40 +0000

    Batched inserts
    Split Inserts into batches of 1000

--- ExceltoSQL/ExceltoSQL/SqlBuilder.cs
+++ ExceltoSQL/ExceltoSQL/SqlBuilder.cs
@@ -45,15 +45,22 @@ namespace ExceltoSQL
                     : ((l.Count() > i) ? l.ElementAt(i) : "null"))
                 .Where((v, i) => Columns.ElementAt(i).Include));
             var sql = new StringBuilder($"IF OBJECT_ID('tempdb..#{TableName}') IS NOT NULL DROP TABLE #{TableName}\r\nGO\r\n\r\n");
-            sql.Append($"CREATE TABLE #{TableName} (tblId int,{selectedColumns.Aggregate(new StringBuilder(), (sb, c) => sb.Append($"[{c.Name}] {c.Type},"))}");
-            sql.Length--;
-            sql.Append($")\r\nINSERT INTO #{TableName} (tblId,{string.Join(", ", selectedColumns.Select(c => $"[{c.Name}]"))})\r\nVALUES\t");
-            sql.Append(string.Join(",\r\n\t\t", selectedValues.Select((l, i) => $"({i+1},{l.Aggregate((t, v) => t + $@",{v}")})")));
-            sql.Append($"\r\n\r\n{selectedColumns.Aggregate(new StringBuilder(), (sb, c) => sb.Append($"DECLARE @{c.Name.Replace(' ', '_').Replace('/', '_')} {c.Type};\r\n"))}");
+            sql.Append($"CREATE TABLE #{TableName} (tblId int identity, {selectedColumns.Aggregate(new StringBuilder(), (sb, c) => sb.Append($"[{c.Name}] {c.Type}, "))}");
+            sql.Length -= 2;
+			sql.Append(")");
+
+			for (var k = 0; k <= _values.Count() / 1000; k++)
+			{
+				sql.Append($"\r\nINSERT INTO #{TableName} ({string.Join(", ", selectedColumns.Select(c => $"[{c.Name}]"))})\r\nVALUES\t");
+				sql.Append(string.Join(",\r\n\t\t", selectedValues.Skip(k * 1000).Take(1000).Select(l => $"({l.Aggregate((t, v) => t + $@",{v}")})")));
+				sql.Append("\r\n");
+			}
+
+			sql.Append($"\r\n\r\n{selectedColumns.Aggregate(new StringBuilder(), (sb, c) => sb.Append($"DECLARE @{c.Name.Replace(' ', '_').Replace('/', '_')} {c.Type};\r\n"))}");
             sql.Append($"DECLARE @MaxId int = {_values.Count()};\r\nDECLARE @CurrentId int = 1;\r\n\r\nWHILE @CurrentId <= @MaxId\r\nBEGIN\r\n\tSELECT TOP 1");
             sql.Append($"{selectedColumns.Aggregate(new StringBuilder(), (sb, c) => sb.Append($"\r\n\t@{c.Name.Replace(' ', '_').Replace('/', '_')} = [{c.Name}],"))}");
             sql.Length--;
-            sql.Append($"\tFROM #{TableName}\r\n\tWHERE TblId = @CurrentId\r\n\r\n\t--insert logic here...\r\n\r\n\tSELECT @CurrentId = @CurrentId + 1\r\nEND");
+            sql.Append($"\tFROM #{TableName}\r\n\tWHERE tblId = @CurrentId\r\n\r\n\t--insert logic here...\r\n\r\n\tSELECT @CurrentId = @CurrentId + 1\r\nEND");
             return sql.ToString();
         }
     }
