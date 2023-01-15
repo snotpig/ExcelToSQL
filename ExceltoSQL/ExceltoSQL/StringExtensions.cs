a9f8c95e27232ec881e4c090e66aae773a563839
Adam Sands <adam.sands@morningstar.com>
2023-01-15 20:46:46 +0000

    gdsgfsd

--- ExceltoSQL/ExceltoSQL/StringExtensions.cs
+++ ExceltoSQL/ExceltoSQL/StringExtensions.cs
@@ -6,9 +6,13 @@ namespace ExceltoSQL
     {
         public static string ToSqlDatetimeString(this string str)
         {
-            return new DateTime(int.Parse(str.Substring(6, 4)), int.Parse(str.Substring(3, 2)),
-                int.Parse(str.Substring(0, 2)), int.Parse(str.Substring(11, 2)), int.Parse(str.Substring(14, 2)), 0)
-                    .ToString("yyyy-MM-dd HH:mm:ss");
+            return new DateTime(
+                int.Parse(str.Substring(6, 4)), 
+                int.Parse(str.Substring(3, 2)),
+                int.Parse(str.Substring(0, 2)), 
+                str.Length > 12 ? int.Parse(str.Substring(11, 2)) : 0,
+                str.Length > 15 ? int.Parse(str.Substring(14, 2)) : 0
+                , 0).ToString("yyyy-MM-dd HH:mm:ss");
 		}
     }
 }
